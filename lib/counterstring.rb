59f69e3abb99db91a453c06668e546a59b6374c4
Shelby Doolittle <shelby@shelbyd.com>
2015-08-24 13:09:43 -0700

    implement counterstring

--- lib/counterstring.rb
+++ lib/counterstring.rb
@@ -1,5 +1,37 @@
 require "counterstring/version"
 
 module Counterstring
-  # Your code goes here...
+  def self.new(*args)
+    String.new(*args)
+  end
+
+  class String < Struct.new(:length)
+    def to_s
+      return '' if length.zero?
+
+      return length.to_s if length == 1
+
+      output = '1' + marker
+      last_position_number = 1
+
+      while output.size < length
+        position_number = output.size + (last_position_number.to_s.size)
+        if position_number.to_s.size > last_position_number.to_s.size
+          position_number += 1
+        end
+        token = position_number.to_s + marker
+        remaining_length = length - output.size
+        output += token
+        last_position_number = position_number
+      end
+
+      output[0,length]
+    end
+
+    private
+
+    def marker
+      '*'
+    end
+  end
 end
--- spec/counterstring_spec.rb
+++ spec/counterstring_spec.rb
@@ -5,7 +5,43 @@ describe Counterstring do
     expect(Counterstring::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#to_s' do
+    subject { Counterstring.new(length).to_s }
+
+    context 'with length 0' do
+      let(:length) { 0 }
+
+      it { is_expected.to eq '' }
+    end
+
+    context 'with length 1' do
+      let(:length) { 1 }
+
+      it { is_expected.to eq '1' }
+    end
+
+    context 'with length 2' do
+      let(:length) { 2 }
+
+      it { is_expected.to eq '1*' }
+    end
+
+    context 'with length 3' do
+      let(:length) { 3 }
+
+      it { is_expected.to eq '1*3' }
+    end
+
+    context 'with length 4' do
+      let(:length) { 4 }
+
+      it { is_expected.to eq '1*3*' }
+    end
+
+    context 'with length 20' do
+      let(:length) { 20 }
+
+      it { is_expected.to eq '1*3*5*7*9*12*15*18*2' }
+    end
   end
 end
